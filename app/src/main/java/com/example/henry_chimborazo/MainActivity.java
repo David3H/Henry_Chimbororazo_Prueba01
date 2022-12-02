93600f538953ad0f8e1d1abda730f8c03f906957
David3H <henrychimborazo331@gmail.com>
2022-12-01 19:15:11 -0500

    19:15

--- app/src/main/java/com/example/henry_chimborazo/MainActivity.java
+++ app/src/main/java/com/example/henry_chimborazo/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.henry_chimborazo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonCerrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // mostrar una segunda pantalla
+                Intent intent = new Intent( MainActivity.this, MainActivitySecond.class);
+            }
+        });
     }
 }
